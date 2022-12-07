7e325439e22e9849041886a926c4c6e3b9a43a96
bobirjon <210030@akfauniversity.org>
2022-12-07 21:07:52 +0500

    update

--- paycom/settings.py
+++ paycom/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x3p**-yq8%u1yn^%z1sug0&vsgswrk-kmq60!e-g=r&sc6$6@x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
