a240675a9f7e79084cf19fbea776414e29a50247
bobirjon <210030@akfauniversity.org>
2022-12-08 18:24:37 +0500

    update

--- payme/urls.py
+++ payme/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from payme.views import MerchantAPIView
-
+from payme.views import MerchantAPIView, SuccessNotifier
 
 urlpatterns = [
-    path('merchant', MerchantAPIView.as_view())
+    path('merchant', MerchantAPIView.as_view()),
+    path('success', SuccessNotifier.as_view())
+
 ]
